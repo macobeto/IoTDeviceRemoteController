6b2b3e4e06e07007f301059add560d2507540eca
Marco <marco.a.marin@live.com>
2016-09-25 10:02:59 -0400

    Initial commit 1.1

--- IoTDeviceRemoteController/src/IoTDeviceRemoteController/Startup.cs
+++ IoTDeviceRemoteController/src/IoTDeviceRemoteController/Startup.cs
@@ -30,7 +30,7 @@ namespace IoTDeviceRemoteController
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!1");
             });
         }
     }
